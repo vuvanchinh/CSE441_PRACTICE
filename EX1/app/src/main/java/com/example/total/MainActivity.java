92b974b8d50d15a68d345abca157bb75ff867d39
vuvanchinh <2151173753@e.tlu.edu.vn>
2024-10-01 15:59:53 +0700

    first commit

---
+++ EX1/app/src/main/java/com/example/total/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.total;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText edtA, edtB, edtKQ;
+    Button btncong;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        edtA = findViewById(R.id.edtA);
+        edtB = findViewById(R.id.edtB);
+        edtKQ = findViewById(R.id.edtKQ);
+        btncong = findViewById(R.id.btntong);
+
+        btncong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int a = Integer.parseInt((edtA.getText().toString()));
+                int b = Integer.parseInt((edtB.getText().toString()));
+                int c = a+b;
+                edtKQ.setText(c+"");
+            }
+        });
+
+    }
+}
